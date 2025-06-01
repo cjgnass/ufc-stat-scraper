e1f25b245f6f781ee7f73940216bc30b437b4a1e
cjgnass <cjgnass@gmail.com>
2025-06-01 19:46:33 -0400

    made scraper
    scraped the URLs for all ufc matches

---
+++ main.py
@@ -0,0 +1,13 @@
+from scrape import scrape_event_urls, scrape_match_data, scrape_match_urls
+
+
+
+starting_url = "http://ufcstats.com/statistics/events/completed?page=all"
+event_urls = scrape_event_urls(starting_url)
+
+match_urls = scrape_match_urls(event_urls[0])
+
+
+
+
+
--- scrape.py
+++ scrape.py
@@ -1,21 +1,78 @@
-import re 
+
 import json
 import requests
+from typing import Optional, List
+from bs4 import BeautifulSoup
 
 
+## Scrape Match Data ## 
+def scrape_match_data(url: str) -> str:
+    """
+    Args:
+        url (str): URL of match event
 
-def scrape_url(url):
-    
+    Returns:
+        (str): data of match 
+    """
     response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    content = find_matchup(soup)
     
+    return content
+
+def find_matchup(soup: BeautifulSoup) -> str:
+    """
+    Args:
+        soup (BeautifulSoup): nested data structure that represents a document
     
+    Returns:
+        (str): matchup
+    """
+    return soup.find('a', class_='b-link').contents
 
-    
+## Scrape URLs ##
+def scrape_event_urls(url: str) -> List[str]:
+    """
+    Args:
+        url (str): URL containing links to events
 
-    return response
+    Returns:
+        event_urls: (List[str]): list of event URLs
+    """
+    event_urls = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    elements = soup.find_all('a', class_='b-link b-link_style_black')
+    for element in elements:
+        event_urls.append(element['href'])
 
-response = scrape_url('http://ufcstats.com/fight-details/b873c116e39c9920')
-print(response)
+    return event_urls
+
+
+
+def scrape_match_urls(url: str) -> List[str]:
+    """
+    Args:
+        url (str): URL containing links to matches
+
+    Returns:
+        match_urls (List[str]): list of match URLs
+    """
+    match_urls = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    elements = soup.find_all('a', class_='b-flag b-flag_style_green')
+    for element in elements:
+        match_urls.append(element['href'])
+
+    print(soup.find('span', class_='b-content__title-highlight').text.strip())
+    return match_urls
+
+
+
+
+
+
 
 
     
